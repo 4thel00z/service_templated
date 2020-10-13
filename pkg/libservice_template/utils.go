abb784873829a3898dfa7941f7a80e320bc38868
4thel00z <4thel00z@gmail.com>
2020-10-13 22:46:10 +0200

    [master] Remove useless function in pkg/libservice_template/utils.go

--- pkg/libservice_template/utils.go
+++ pkg/libservice_template/utils.go
@@ -1,7 +1,6 @@
 package libservice_template
 
 import (
-	"fmt"
 	"github.com/monzo/typhon"
 )
 
@@ -12,11 +11,4 @@ func Default404Handler(app App) typhon.Service {
 		response.StatusCode = 404
 		return response
 	}
-}
-
-func VerifyRequest(r *typhon.Request) error {
-	if _, ok := r.MultipartForm.File["audio"]; !ok || len(r.MultipartForm.File["audio"]) < 1 {
-		return fmt.Errorf("the field audio is absent")
-	}
-	return nil
 }
