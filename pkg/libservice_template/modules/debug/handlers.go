75c7927d732d7aedcea346485b55ff0d462a5318
4thel00z <4thel00z@gmail.com>
2020-11-15 19:38:58 +0100

    [master] Remove GetPrivateMessageHandler from the debug module

--- pkg/libservice_template/modules/debug/handlers.go
+++ pkg/libservice_template/modules/debug/handlers.go
@@ -1,11 +1,8 @@
 package debug
 
 import (
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/monzo/typhon"
 	"service_templated/pkg/libservice_template"
-	libjwt "service_templated/pkg/libservice_template/jwt"
 )
 
 func GetRoutesHandler(app libservice_template.App) typhon.Service {
@@ -15,17 +12,6 @@ func GetRoutesHandler(app libservice_template.App) typhon.Service {
 			Routes: app.Routes(),
 		})
 
-		response.StatusCode = 200
-		return response
-	}
-}
-
-func GetPrivateMessageHandler(app libservice_template.App) typhon.Service {
-	return func(req typhon.Request) typhon.Response {
-		token := req.Value(libjwt.DefaultUserProperty).(*jwt.Token)
-		response := req.Response(&libservice_template.GenericResponse{
-			Message: fmt.Sprintf("This is my token: %s!", token.Raw),
-		})
 		response.StatusCode = 200
 		return response
 	}
